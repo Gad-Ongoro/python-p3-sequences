f484e12c91342cf8537a3f1d40a8cc51bd0c2b8c
Gad-Ongoro <gadongoro1@gmail.com>
2023-11-25 10:29:21 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence = [0, 1]
+    # fibonacci_sequence = []
+
+    if length == 0:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+
+    while len(fibonacci_sequence) < length:        
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+                    
+    print(fibonacci_sequence)
     pass
+
+print_fibonacci(0)
